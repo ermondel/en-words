5b664258763a04ac214411f7bd60a50b28762cbc
ermondel <ermondel@gmail.com>
2018-11-22 18:38:14 +0200

    feat: Add data file with vocabulary to app and test its basic rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import dataVocabulary from './dataVocabulary'
 
 export default class App extends Component {
     render() {
+        let content = null;
+        if (dataVocabulary) {
+            content = dataVocabulary.map(word => <p key={word.id}>{word.ukr + ' / '+ word.eng}</p>);
+        }
+
         return (
-            <div className="App">
-                english-prompts
-            </div>
+            <div className="App">{content}</div>
         )
     }
 }
