c06ac65b06def7df2086571b2c3b223c2e7f653f
ermondel <ermondel@gmail.com>
2018-11-24 18:19:52 +0200

    feat: Add state variable to store current word id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import dataVocabulary from './dataVocabulary';
-import WordCard from './WordCard'
+import WordCard from './WordCard';
 
 export default class App extends Component {
+    state = {
+        currentWord: 142
+    }
+
     render() {
+        const { currentWord } = this.state
         let content = null;
         // if (dataVocabulary) content = dataVocabulary.map(word => <p key={word.id}>{word.ukr + ' / '+ word.eng}</p>);
         
+        if (dataVocabulary[currentWord]) {
+            console.log(dataVocabulary[currentWord])
+        } else {
+            console.log('FAIL :-(');
+        }
+
         content = <WordCard ukr={'ukr'} eng={'eng'} type={'type'} />
 
         return (
