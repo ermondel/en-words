59662aa34b7889c16c1db89d4834cd9411b8b3fc
ermondel <ermondel@gmail.com>
2018-11-26 15:32:09 +0200

    feat: add class names for blocks of the WordCard component

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,13 +3,13 @@ import './WordCard.css';
 
 const WordCard = (props) => (
     <div className="WordCard">
-        <div>{props.pos} / {props.total}</div>
-        <div>{props.ukr}</div>
-        <div>{props.eng}</div>
-        <div>{props.type}</div>
+        <div className="pos">{props.pos} / {props.total}</div>
+        <div className="ukr">{props.ukr}</div>
+        <div className="type">{props.type}</div>
+        <div className="eng">{props.eng}</div>
         <form onSubmit={props.onSubmit}>
-            <input type="text" name="translation" />
-            <button>Check</button>
+            <input type="text" name="translation" className="translation" />
+            <button className="btn-check">Check</button>
         </form>
     </div>
 )
