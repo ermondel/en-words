4d480b6092cbae160ab126882d828c7f5e29c8d7
ermondel <ermondel@gmail.com>
2018-12-08 11:10:01 +0200

    feat: add routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Vocabulary from './components/vocabulary/Vocabulary'
 import Sentence from './components/sentence/Sentence';
 
-const App = () => <Vocabulary />
+const App = () => (
+    <Router>
+        <div className="App">
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/vocabulary/">Vocabulary</Link></li>
+                        <li><Link to="/sentence/">Sentence</Link></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/vocabulary/" component={Vocabulary} />
+            <Route path="/sentence/" component={Sentence} />
+        </div>
+    </Router>
+);
+
+const Index = () => <h2>Home</h2>;
 
 export default App;
