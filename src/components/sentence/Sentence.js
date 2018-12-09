8e5fafb4137d5d36e40d87ae66979add39c46ef8
ermondel <ermondel@gmail.com>
2018-12-09 18:54:47 +0200

    feat: add states for the Sentence component

--- src/components/sentence/Sentence.js
+++ src/components/sentence/Sentence.js
@@ -4,6 +4,12 @@ import data from './data-fixture';
 import './style/Sentence.css';
 
 class Sentence extends Component {
+    state = {
+        index: data.length - 1,
+        incorrect: false,
+        value: ""
+    }
+
     render() {
         return (<div className="Sentence"><h2>Sentence</h2></div>)
     }
