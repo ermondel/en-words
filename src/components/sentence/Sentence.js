07701e2037503f8779d1a55d2990c0e2f198d2d0
ermondel <ermondel@gmail.com>
2018-12-09 19:13:54 +0200

    feat: add code to the Sentence component render

--- src/components/sentence/Sentence.js
+++ src/components/sentence/Sentence.js
@@ -10,8 +10,30 @@ class Sentence extends Component {
         value: ""
     }
 
+    forRender = null
+
     render() {
-        return (<div className="Sentence"><h2>Sentence</h2></div>)
+        let { index } = this.state;
+
+        if (data[index]) {
+            this.forRender = <SentenceCard
+                ukr       = {data[index].ukr}
+                eng       = {data[index].eng}
+                type      = {data[index].type}
+                pos       = {data.length - index}
+                total     = {data.length}
+                onSubmit  = {null}
+                incorrect = {this.state.incorrect}
+                value     = {this.state.value}
+                changed   = {null}
+            />
+        } else if (index < 0) {
+            console.log('End of sentences');
+        } else {
+            console.log('Sentence not found');
+        }
+
+        return (<div className="Sentence">{this.forRender}</div>)
     }
 }
 
