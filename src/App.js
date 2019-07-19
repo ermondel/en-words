cb8384f9f66817f872e3bd53e1e2cf10b218ae14
Ermondel <ermondel@gmail.com>
2019-07-19 11:45:34 +0300

    feat: expand states and set props for components

--- src/App.js
+++ src/App.js
@@ -6,17 +6,52 @@ import words from './tests/fixtures/words';
 
 class App extends Component {
     state = {
-        wordsList: [],
+        wordsList: words,
         wordСorrectly: false,
-        wordID: 0
+        wordIndex: 0,
+        wordInput: '',
+        wordAnswer: '',
+        showAnswer: false,
+        wordsCounter: 0
+    }
+
+    get currentWord() {
+        const currentWord = this.state.wordsList[this.state.wordIndex];
+        return currentWord ? currentWord.ru : '';
+    }
+
+    onUserInputChange = (event) => {
+        console.log('onUserInputChange');
+    }
+
+    onUserInputFocus = (event) => {
+        console.log('onUserInputFocus');
+    }
+
+    onShowAnswerClick = () => {
+        console.log('onShowAnswerClick');
     }
 
     render() {
         return(
             <div>
-                <WordToTranslate />
-                <UserInput />
-                <Toolbar />
+                <WordToTranslate
+                    word={ this.currentWord }
+                    highlight={ this.wordСorrectly } 
+                />
+                <UserInput
+                    word={ this.state.wordInput }
+                    answer={ this.state.wordAnswer }
+                    showAnswer={ this.state.showAnswer }
+                    onChange= { this.onUserInputChange }
+                    onFocus={ this.onUserInputFocus }
+                />
+                <Toolbar
+                    wordСorrectly={ this.state.wordСorrectly } 
+                    onShowAnswerClick={ this.onShowAnswerClick }
+                    totalWords={ this.state.wordsList.length }
+                    currentPosition={ this.state.wordIndex + 1 }
+                />
             </div>
         );
     }
