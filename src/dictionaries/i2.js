b5a946463111ab30d13a62d98f298169b87141f8
Ermondel <ermondel@gmail.com>
2019-09-05 20:33:06 +0300

    feat: add 34 new words to the dictionary

--- src/dictionaries/i2.js
+++ src/dictionaries/i2.js
@@ -1,7 +1,72 @@
 export default [
   {
-    en: 'test 1',
-    ru: 'тест 1',
+    en: 'broadcast',
+    ru: 'вещание, трансляция, передача, транслировать',
+    type: 'noun/verb'
+  },
+  {
+    en: 'plot',
+    ru: 'сюжет, заговор, график, участок земли, замышлять',
+    type: 'noun/verb'
+  },
+  { en: 'episode', ru: 'эпизод, серия', type: 'noun' },
+  { en: 'cabaret', ru: 'кабаре', type: 'noun' },
+  { en: 'host', ru: 'принимать, провести, хозяин, ведущий', type: 'verb/noun' },
+  { en: 'sitcom', ru: 'ситком, комедия', type: 'verb/noun' },
+  {
+    en: 'interview',
+    ru: 'интервью, собеседование, брать интервью',
+    type: 'noun/verb'
+  },
+  { en: 'cartoon', ru: 'мультфильм, карикатура', type: 'noun' },
+  { en: 'slow TV', ru: 'Медленное телевидение', type: 'phrase' },
+  { en: 'hidden camera', ru: 'скрытая камера', type: 'phrase' },
+  { en: 'satire', ru: 'сатира', type: 'noun' },
+  { en: 'tearjerker', ru: 'слезливый фильм', type: 'noun' },
+  { en: 'practical joke', ru: 'розыгрыш, грубая шутка', type: 'phrase' },
+  { en: 'TV series', ru: 'телесериал', type: 'noun' },
+  { en: 'cookery', ru: 'кулинария', type: 'noun' },
+  {
+    en: 'documentary',
+    ru: 'документальный фильм, документальный',
+    type: 'noun/adjective'
+  },
+  { en: 'makeover', ru: 'макияж, преображение', type: 'noun' },
+  { en: 'DIY', ru: '"сделай сам"', type: 'phrase' },
+  { en: 'remote control', ru: 'дистанционное управление', type: 'noun' },
+  { en: 'clicker', ru: 'пульт, кликер', type: 'noun' },
+  { en: 'flatscreen TV', ru: 'телевизор с плоским экраном', type: 'phrase' },
+  { en: 'widescreen', ru: 'широкоэкранный', type: 'adjective' },
+  { en: 'premium station', ru: 'премиум станция', type: 'phrase' },
+  { en: 'prime time', ru: 'прайм-тайм', type: 'noun' },
+  {
+    en: 'rerun',
+    ru: 'повтор, повторный показ, перезапускать',
+    type: 'noun/verb'
+  },
+  { en: 'rating', ru: 'рейтинг, оценка, рейтинговый', type: 'noun/verb' },
+  { en: 'newscaster', ru: 'диктор, ведущий новостей', type: 'noun' },
+  { en: 'newscast anchor', ru: 'ведущий новостей', type: 'noun' },
+  { en: 'commercial break', ru: 'рекламная пауза', type: 'noun' },
+  {
+    en: 'decay',
+    ru: 'распад, упадок, разложение, распадаться, разлагаться',
+    type: 'noun/verb'
+  },
+  { en: 'attempt', ru: 'попытка, пытаться', type: 'noun/verb' },
+  {
+    en: 'bid',
+    ru: 'заявка, предложение, ставка, предложить, поставить',
+    type: 'noun/verb'
+  },
+  {
+    en: 'storage locker',
+    ru: 'шкафчик для хранения, хранилище',
     type: 'phrase'
+  },
+  {
+    en: 'decrease',
+    ru: 'сокращение, снижение, уменьшить, снизить',
+    type: 'noun/verb'
   }
 ];
