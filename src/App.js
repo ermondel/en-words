f7c04cafe44f593446cdaba585cbe4e93c86240b
Ermondel <ermondel@gmail.com>
2019-09-05 18:57:55 +0300

    feat: add dictionary for another level

--- src/App.js
+++ src/App.js
@@ -4,113 +4,114 @@ import UserInput from './components/UserInput';
 import Toolbar from './components/Toolbar';
 import DeadEnd from './components/DeadEnd';
 import './styles/styles.scss';
-import words from './dictionaries/base';
+import words from './dictionaries/i2';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.refButtonNext = React.createRef();
-        this.refUserInput = React.createRef();
+    this.refButtonNext = React.createRef();
+    this.refUserInput = React.createRef();
+  }
+
+  state = {
+    wordsList: words,
+    wordСorrectly: false,
+    wordIndex: 0,
+    wordInput: '',
+    showAnswer: false,
+    wordsCounter: 0
+  };
+
+  get currentWord() {
+    const currentWord = this.state.wordsList[this.state.wordIndex];
+    return currentWord ? currentWord.ru : '';
+  }
+
+  get wordAnswer() {
+    const currentWord = this.state.wordsList[this.state.wordIndex];
+    return currentWord ? currentWord.en : '';
+  }
+
+  onUserInputChange = event => {
+    const currentWord = this.state.wordsList[this.state.wordIndex];
+    const wordInput = event.target.value;
+    const wordСorrectly =
+      currentWord && currentWord.en && currentWord.en === wordInput;
+
+    if (wordСorrectly) {
+      const buttonNext = this.refButtonNext.current;
+      setTimeout(() => buttonNext.focus(), 1);
     }
 
-    state = {
-        wordsList: words,
-        wordСorrectly: false,
-        wordIndex: 0,
-        wordInput: '',
-        showAnswer: false,
-        wordsCounter: 0
-    }
+    this.setState(() => ({ wordInput, wordСorrectly }));
+  };
 
-    get currentWord() {
-        const currentWord = this.state.wordsList[this.state.wordIndex];
-        return currentWord ? currentWord.ru : '';
-    }
+  onUserInputFocus = () => {
+    this.setState(() => ({ showAnswer: false }));
+  };
 
-    get wordAnswer() {
-        const currentWord = this.state.wordsList[this.state.wordIndex];
-        return currentWord ? currentWord.en : '';
-    }
+  onShowAnswerClick = () => {
+    this.setState(() => ({ showAnswer: true }));
+  };
 
-    onUserInputChange = event => {
-        const currentWord   = this.state.wordsList[this.state.wordIndex];
-        const wordInput     = event.target.value;
-        const wordСorrectly = currentWord && currentWord.en && currentWord.en === wordInput;
+  onButtonNextClick = () => {
+    this.setState(prevState => ({
+      wordСorrectly: false,
+      wordIndex: prevState.wordIndex + 1,
+      wordInput: '',
+      showAnswer: false,
+      wordsCounter: prevState.wordsCounter + 1
+    }));
+    this.refUserInput.current.focus();
+  };
 
-        if (wordСorrectly) {
-            const buttonNext = this.refButtonNext.current;
-            setTimeout(() => buttonNext.focus(), 1);
-        }
+  onReloadClick = () => {
+    this.setState(() => ({
+      wordСorrectly: false,
+      wordIndex: 0,
+      wordInput: '',
+      showAnswer: false,
+      wordsCounter: 0
+    }));
+  };
 
-        this.setState(() => ({ wordInput, wordСorrectly }));
-    }
-
-    onUserInputFocus = () => {
-        this.setState(() => ({ showAnswer: false }));
-    }
-
-    onShowAnswerClick = () => {
-        this.setState(() => ({ showAnswer: true }));
-    }
-
-    onButtonNextClick = () => {
-        this.setState(prevState => ({ 
-            wordСorrectly: false, 
-            wordIndex: prevState.wordIndex + 1,
-            wordInput: '',
-            showAnswer: false,
-            wordsCounter: prevState.wordsCounter + 1
-        }));
-        this.refUserInput.current.focus();
-    }
-
-    onReloadClick = () => {
-        this.setState(() => ({
-            wordСorrectly: false,
-            wordIndex: 0,
-            wordInput: '',
-            showAnswer: false,
-            wordsCounter: 0
-        }));
-    }
-
-    render() {
-        return (
-            <div className="App">
-            {
-                this.state.wordsCounter < this.state.wordsList.length ? (
-                    <div className="workspace">
-                        <WordToTranslate
-                            word={ this.currentWord }
-                            highlight={ this.state.wordСorrectly } 
-                        />
-                        <UserInput
-                            word={ this.state.wordInput }
-                            answer={ this.wordAnswer }
-                            showAnswer={ this.state.showAnswer }
-                            onChange= { this.onUserInputChange }
-                            onFocus={ this.onUserInputFocus }
-                            wordСorrectly={ this.state.wordСorrectly } 
-                            refUserInput={ this.refUserInput }
-                        />
-                        <Toolbar
-                            wordСorrectly={ this.state.wordСorrectly } 
-                            onShowAnswerClick={ this.onShowAnswerClick }
-                            totalWords={ this.state.wordsList.length }
-                            currentPosition={ this.state.wordsCounter + 1 }
-                            onButtonNextClick={ this.onButtonNextClick }
-                            refButtonNext={ this.refButtonNext }
-                        />
-                    </div>
-                ) : <DeadEnd 
-                    wordsListLength = { this.state.wordsList.length } 
-                    onReloadClick = { this.onReloadClick }
-                />
-            }
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div className="App">
+        {this.state.wordsCounter < this.state.wordsList.length ? (
+          <div className="workspace">
+            <WordToTranslate
+              word={this.currentWord}
+              highlight={this.state.wordСorrectly}
+            />
+            <UserInput
+              word={this.state.wordInput}
+              answer={this.wordAnswer}
+              showAnswer={this.state.showAnswer}
+              onChange={this.onUserInputChange}
+              onFocus={this.onUserInputFocus}
+              wordСorrectly={this.state.wordСorrectly}
+              refUserInput={this.refUserInput}
+            />
+            <Toolbar
+              wordСorrectly={this.state.wordСorrectly}
+              onShowAnswerClick={this.onShowAnswerClick}
+              totalWords={this.state.wordsList.length}
+              currentPosition={this.state.wordsCounter + 1}
+              onButtonNextClick={this.onButtonNextClick}
+              refButtonNext={this.refButtonNext}
+            />
+          </div>
+        ) : (
+          <DeadEnd
+            wordsListLength={this.state.wordsList.length}
+            onReloadClick={this.onReloadClick}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/dictionaries/i2.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    en: 'test 1',
+    ru: 'тест 1',
+    type: 'phrase'
+  }
+];
